a0af28241e2315b45d921b5c86ba576402b7a847
ralph <csw@semantic-web.ro>
2014-05-27 15:47:20 +0200

    Revert "Merge pull request #8 from greenTara/recursive"
    This reverts commit 6b54879f2ddf17331c4c098ea77a8d6814794e2e, reversing
    changes made to b4be835a68eb8c9d38961042793ea35fb7449a77.

--- OntoMaven-CL/src/main/java/de/csw/cl/importer/model/Corpus.java
+++ OntoMaven-CL/src/main/java/de/csw/cl/importer/model/Corpus.java
@@ -5,8 +5,6 @@ package de.csw.cl.importer.model;
 
 import static util.XMLUtil.NS_XCL2;
 
-import java.io.File;
-import java.io.FilenameFilter;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -31,163 +29,108 @@ import util.XMLUtil;
  * 
  */
 public class Corpus {
+	
+	private static Logger LOG = Logger.getLogger(Corpus.class);
 
-    public Boolean hasResolvableImports;
+	private final Document baseDocument;
+	private final HashSet<Document> documents = new HashSet<Document>();
+	
 
-    private static Logger LOG = Logger.getLogger(Corpus.class);
+	// All top-level titlings (by name) that are available for importation.
+	// During the importation process, new titlings, which become importable
+	// because their surrounding titling has been imported, might be added to
+	// this map.
+	private HashMap<String, Element> importableTitlings = new HashMap<String, Element>();;
 
-    private File baseDir;
-    private HashSet<Document> documents = new HashSet<Document>();
-    private Includes includes;
-    private XMLCatalog catalog;
+	/**
+	 * 
+	 * @param baseDocument
+	 * @param documents
+	 * @throws ConflictingTitlingException 
+	 */
+	public Corpus(Document baseDocument) throws ConflictingTitlingException {
+		this.baseDocument = baseDocument;
+		addDocument(baseDocument);
+	}
 
-    // All top-level titlings (by name) that are available for importation.
-    // During the importation process, new titlings, which become importable
-    // because their surrounding titling has been imported, might be added to
-    // this map.
-    private HashMap<String, Element> importableTitlings = new HashMap<String, Element>();;
+	/**
+	 * Adds a loaded document to the corpus.
+	 * 
+	 * @param doc
+	 *            the document to add.
+	 * @throws ConflictingTitlingException 
+	 */
+	public void addDocument(Document doc) throws ConflictingTitlingException {
+		documents.add(doc);
+		extractTitlings(doc.getRootElement());
+	}
+	
+	/**
+	 * Returns the base document.
+	 * @return
+	 */
+	public Document getBaseDocument() {
+		return baseDocument;
+	}
+	
+	/**
+	 * Returns an importable titling with a given name, or null if no such
+	 * titling exists.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public Element getImportableTitling(String name) {
+		return importableTitlings.get(name);
+	}
+	
+	/**
+	 * Performs a recursive depth-first traversion of the subtree under the
+	 * given element e for Titling elements and adds the found Titling elements
+	 * to the map of importable Titlings. The search will not continue under the
+	 * found Titling elements.
+	 * 
+	 * @param e
+	 * @throws ConflictingTitlingException 
+	 */
+	private void extractTitlings(Element e) throws ConflictingTitlingException {
+		List<Element> childElements = e.getChildren();
+		for (Element element : childElements) {
+			if (element.getName().equals("Titling")) {
+				addImportableTitling(element);
+			} else {
+				extractTitlings(element);
+			}
+		}
+	}
 
-    /**
-     * 
-     * @param baseDir
-     * @param includes
-     * @param catalog
-     * @throws ConflictingTitlingException
-     */
-    public Corpus(File baseDir, Includes includes, XMLCatalog catalog)
-            throws ConflictingTitlingException {
-        this.baseDir = baseDir;
-        File inputDir = new File(baseDir, "input");
-        this.includes = includes;
-        this.catalog = catalog;
-        File[] files = inputDir.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.toLowerCase().endsWith(".xcl");
-            }
-        });
+	/**
+	 * Adds an importable titling to the map of importable titlings.
+	 * @param e
+	 * @throws ConflictingTitlingException 
+	 */
+	private void addImportableTitling(Element e)
+			throws ConflictingTitlingException {
+		Element nameElement = e.getChild("Name", NS_XCL2);
+		String titlingName = nameElement.getAttributeValue("cri");
 
-        for (File file : files) {
-            Document doc = XMLUtil.readLocalDoc(file);
-            this.addDocument(doc);
-        }
-        hasResolvableImports = true;
-
-    }
-
-    /**
-     * Clones an existing corpus.
-     * 
-     * @param corpus
-     */
-    public Corpus(Corpus corpus) {
-        this.baseDir = corpus.baseDir;
-        this.includes = corpus.includes;
-        this.catalog = corpus.catalog;
-        this.documents = corpus.documents;
-        this.importableTitlings = corpus.importableTitlings;
-        this.hasResolvableImports = corpus.hasResolvableImports;
-    }
-
-    /**
-     * Adds a loaded document to the corpus.
-     * 
-     * @param doc
-     *            the document to add.
-     * @throws ConflictingTitlingException
-     */
-    public void addDocument(Document doc) throws ConflictingTitlingException {
-        documents.add(doc);
-        extractTitlings(doc.getRootElement());
-    }
-
-    /**
-     * Returns the documentses.
-     * 
-     * @return
-     */
-    public HashSet<Document> getDocuments() {
-        return documents;
-    }
-
-    /**
-     * Returns the includes.
-     * 
-     * @return
-     */
-    public Includes getIncludes() {
-        return includes;
-    }
-
-    /**
-     * Returns the catalog.
-     * 
-     * @return
-     */
-    public XMLCatalog getXMLCatalog() {
-        return catalog;
-    }
-
-    /**
-     * Returns an importable titling with a given name, or null if no such
-     * titling exists.
-     * 
-     * @param name
-     * @return
-     */
-    public Element getImportableTitling(String name) {
-        return importableTitlings.get(name);
-    }
-
-    /**
-     * Performs a recursive depth-first traversion of the subtree under the
-     * given element e for Titling elements and adds the found Titling elements
-     * to the map of importable Titlings. The search will not continue under the
-     * found Titling elements.
-     * 
-     * @param e
-     * @throws ConflictingTitlingException
-     */
-    private void extractTitlings(Element e) throws ConflictingTitlingException {
-        List<Element> childElements = e.getChildren();
-        for (Element element : childElements) {
-            if (element.getName().equals("Titling")) {
-                addImportableTitling(element);
-            } else {
-                extractTitlings(element);
-            }
-        }
-    }
-
-    /**
-     * Adds an importable titling to the map of importable titlings.
-     * 
-     * @param e
-     * @throws ConflictingTitlingException
-     */
-    private void addImportableTitling(Element e)
-            throws ConflictingTitlingException {
-        Element nameElement = e.getChild("Name", NS_XCL2);
-        String titlingName = nameElement.getAttributeValue("cri");
-
-        Element existingImportableTitling = importableTitlings.get(titlingName);
-        if (existingImportableTitling != null) {
-            if (!XMLUtil.equal(e, existingImportableTitling)) {
-                // conflicting Titling
-                throw new ConflictingTitlingException(titlingName,
-                        existingImportableTitling, e);
-            } else {
-                System.out.println("  Titling " + titlingName
-                        + " exists. Skipping.");
-            }
-        } else {
-            System.out.println("  Adding titling " + titlingName);
-
-            // store the titling, remove surrounding titling element
-            // if it has more than one direct child, put a Construct element
-            // around them
-
-            importableTitlings.put(titlingName, e);
-        }
-    }
-
+		Element existingImportableTitling = importableTitlings.get(titlingName);
+		if (existingImportableTitling != null) {
+			if (!XMLUtil.equal(e, existingImportableTitling)) {
+				// conflicting Titling
+				throw new ConflictingTitlingException(titlingName,
+						existingImportableTitling, e);
+			} else {
+				System.out.println("  Titling " + titlingName + " exists. Skipping.");
+			}
+		} else {
+			System.out.println("  Adding titling " + titlingName);
+			
+			// store the titling, remove surrounding titling element
+			// if it has more than one direct child, put a Construct element around them
+			
+			importableTitlings.put(titlingName, e);
+		}
+	}
+	
 }
